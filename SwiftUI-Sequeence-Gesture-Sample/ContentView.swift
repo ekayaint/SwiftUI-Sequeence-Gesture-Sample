1bc3b98478b365ea1a484b0ea8b47d3ebbf1b813
ekayaint <ekayaint@gmail.com>
2023-05-08 21:27:11 +0300

    Initial Commit

---
+++ SwiftUI-Sequeence-Gesture-Sample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-Sequeence-Gesture-Sample
+//
+//  Created by Erhan Kaya on 8.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Sequeence-Gesture-Sample/SwiftUI_Sequeence_Gesture_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Sequeence_Gesture_SampleApp.swift
+//  SwiftUI-Sequeence-Gesture-Sample
+//
+//  Created by Erhan Kaya on 8.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Sequeence_Gesture_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
